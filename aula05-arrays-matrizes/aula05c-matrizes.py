cd1bbb6acf35b9ec1650b00c2fe78cee42187fea
Matheusbg1903 <mb19031204@gmail.com>
2026-09-09 20:52:53 -0300

    aua05: arrays e matrizes

---
+++ aula05-arrays-matrizes/aula05c-matrizes.py
@@ -0,0 +1,10 @@
+musicas = [
+    ["Baby"], ["Justin Bienber"],
+    ["brella"], ["Rihanna"],
+    ["Mágica"], ["Calcinha Preta"]
+]
+
+for musicas in musicas:
+    for info in musicas:
+        print(info)
+    print()
